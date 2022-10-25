d36071ed32d4424a6fd2f7ec7f5220fee614f814
Diego Alves <diegoteialves@gmail.com>
2022-10-24 22:01:01 -0300

    Número 4, 5, 6 e 7 já resolvido

--- CalculadoraDoDev.js
+++ CalculadoraDoDev.js
@@ -24,13 +24,48 @@ var numeroB = 0
 numeroA = parseInt(prompt("Informe o 1° número:"))
 numeroB = parseInt(prompt("Informe o 2° número:"))
 
-var soma = Somar(numeroA, numeroB)
-var sub  = Sub(numeroA, numeroB)
-var multi = Multi(numeroA, numeroB)
+var escolha = 0
+var condicao = 1
+var arraysoma = []
+var arraysub = []
+var arraymulti = []
+var arraydiv = []
+var indexsoma = 0
+var indexsub = 0
+var indexmulti = 0
+var indexdiv = 0
 
-alert("A soma dos dois número é: " + soma)
-alert("A subtração dos dois número é: " + sub)
-alert("A multiplicação dos dois número é: " + multi)
+while(condicao ==  1){
+
+            escolha = parseInt(prompt("Escolha qual operação deseja realizar: 1 = Somar; 2 = Subtrair; 3 = Multiplicar; 4 = Dividir"))
+
+            if(escolha == 1){
+                arraysoma[indexsoma] = Somar(numeroA, numeroB)
+                alert("A soma dos dois número é: " + arraysoma[indexsoma])
+                indexsoma++
+            }
+
+            if(escolha == 2){
+                arraysub[indexsub]  = Sub(numeroA, numeroB)
+                alert("A subtração dos dois número é: " + arraysub[indexsub])
+                indexsub++
+            }
+
+            if(escolha == 3){
+                arraymulti[arraymulti] = Multi(numeroA, numeroB)
+                alert("A multiplicação dos dois número é: " + arraymulti[arraymulti])
+                indexmulti++
+            }
+
+            if(escolha == 4){
+                arraydiv[indexdiv] = Div(numeroA, numeroB)
+                alert("A divisão dos dois número é: " +  arraydiv[indexdiv])
+                indexdiv++
+            }
+    condicao = prompt("Deseja continuar? sim = 1 e nao = 2")
+}
+
+exibirHistorico(arraydiv, arraymulti, arraysoma, arraysub)
 
 function Somar(num1, num2){
     var soma = (num1 + num2)
@@ -46,4 +81,18 @@ function Multi(num1, num2){
     var multi = (num1 * num2)
     return multi
 }
+
+function Div(num1, num2){
+    var div = (num1 / num2)
+    return div
+}
+
+function exibirHistorico(arraydiv, arraymulti, arraysoma, arraysub){
+   console.log("Historico do resultado das somas: " + arraysoma)
+   console.log("Historico do resultado das subtrações: " + arraysub)
+   console.log("Historico do resultado das divisões: " + arraydiv)
+   console.log("Historico do resultado das multiplicações: " + arraymulti)
+
+}
+
 
